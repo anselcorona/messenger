ce9104431d2f7440d29e045ef852c5805f14f899
anselcorona <anselcorona@gmail.com>
2018-05-09 10:20:22 -0400

    been trying to solve that issue

--- src/main/java/com/ansxl/messenger/resources/CommentResource.java
+++ src/main/java/com/ansxl/messenger/resources/CommentResource.java
@@ -1,14 +1,27 @@
 package com.ansxl.messenger.resources;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CommentResource {
+	
+	
 	
 	@GET
 	public String test(){
 		return "testwww";
+	}
+	
+	@GET
+	@Path("/{commentId}")
+	public String test2(@PathParam("commentID") int commentID) {
+		return "Method to return commentID " + commentID;
 	}
 
 }
